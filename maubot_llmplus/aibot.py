decf1a24662f7da74a853aff273f6f6655f95ad0
taylor <=>
2024-10-12 17:40:35 +0800

    update: 修改模块结构

--- maubot_llmplus/aibot.py
+++ maubot_llmplus/aibot.py
@@ -38,8 +38,8 @@ class AiBotPlugin(Plugin):
         # 加载并更新配置
         self.config.load_and_update()
         # 决定当前机器人的名称
-        self.name = self.config["name"] or \
-                    self.client.get_displayname(self.client.mxid) or \
+        self.name = self.config['name'] or \
+                    await self.client.get_displayname(self.client.mxid) or \
                     self.client.parse_user_id(self.client.mxid)[0]
 
     """
