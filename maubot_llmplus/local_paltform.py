4e936b35b29bb517b55f6e746f4e423faaa2ac68
taylor <taylorxie18@gmail.com>
2024-10-13 18:06:02 +0800

    add: 添加ollama调用AI chat逻辑

--- maubot_llmplus/local_paltform.py
+++ maubot_llmplus/local_paltform.py
@@ -28,8 +28,8 @@ class Ollama(Platform):
         if self.api_key is not None:
             headers['Authorization'] = self.api_key
         plugin.log.debug(f"{json.dumps(req_body)}")
-        async with self.http.post(endpoint, headers=headers, data=json.dumps(req_body)) as response:
-            plugin.log.debug(f"响应内容：{response.status}, {await response.json()}")
+        async with self.http.post(endpoint, headers=headers, json=req_body) as response:
+            # plugin.log.debug(f"响应内容：{response.status}, {await response.json()}")
             if response.status != 200:
                 return ChatCompletion(
                     message={},
